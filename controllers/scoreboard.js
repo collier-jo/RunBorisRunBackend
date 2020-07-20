e194036452e4c354c04d5f7e6bbe833a3d96b94a
joannacollier <collj035@gmail.com>
2020-07-20 13:06:51 +0100

    Controller set up hello world

---
+++ controllers/scoreboard.js
@@ -0,0 +1,12 @@
+var ScoreboardController = {
+  Hello: function(request, response) {
+    res.send("Hello World!")
+
+  },
+};
+  
+module.exports = ScoreboardController;
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// install mongoose 
+// npm install mongoose --save
