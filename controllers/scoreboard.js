43f914bf622d8d180f8abd248ee20c41907bfba8
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-07-21 10:48:17 +0100

    Fixed typo

--- controllers/scoreboard.js
+++ controllers/scoreboard.js
@@ -1,18 +1,18 @@
 var ScoreboardController = {
   Hello: function(request, response) {
-    res.send("Hello World!")
+    response.send("Hello World!")
 
   },
   View: function(request, response){
-    
+
   },
   Create: function(request, response){
-    
+
   }
 };
-  
+
 module.exports = ScoreboardController;
 
 // app.get('/', (req, res) => res.send('Hello World!'))
-// install mongoose 
+// install mongoose
 // npm install mongoose --save
