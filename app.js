58d0ff3bd9abc7d89801d61dc1d8c91723334226
joannacollier <collj035@gmail.com>
2020-08-03 18:24:42 +0100

    Removed heroku, reverted back to orginal before deploy tests

--- app.js
+++ app.js
@@ -6,21 +6,11 @@ const bodyParser = require('body-parser')
 const app = express()
 
 
-// mongoose.connect("mongodb://localhost/run_boris_run", { useNewUrlParser: "true" })
+mongoose.connect("mongodb://localhost/run_boris_run", { useNewUrlParser: "true" })
 
-mongoose.connect("mongodb://localhost/run_boris_run", {useNewUrlParser:"true"}).catch(function (reason) {
-  console.log('Unable to connect to the mongodb instance. Error: ', reason);
-});
-
-
-
-// var url = 'mongodb://localhost:27017/run_boris_run'
-// mongoose.connect(url)
-//   .then(function (db) { // <- db as first argument
-//     console.log(db)
-//   })
-//   .catch(function (err) {})
-
+// mongoose.connect("mongodb://localhost/run_boris_run", {useNewUrlParser:"true"}).catch(function (reason) {
+//   console.log('Unable to connect to the mongodb instance. Error: ', reason);
+// });
 
 mongoose.connection.on("error", err => {
   console.log("err", err)
@@ -42,5 +32,5 @@ var ScoreController = require('./controllers/scoreboard');
 app.get('/', ScoreController.View)
 app.post('/new', ScoreController.Create)
 
-// app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`))
+// app.listen(process.env.PORT || 3000);
