cdd4c2637a1cfcb24f732e5651cb01bc6af44cb4
joannacollier <collj035@gmail.com>
2020-08-03 16:42:32 +0100

    catch added

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const bodyParser = require('body-parser')
 const app = express()
 
 
-mongoose.connect("mongodb://localhost/run_boris_run", { useNewUrlParser: "true" })
+// mongoose.connect("mongodb://localhost/run_boris_run", { useNewUrlParser: "true" })
 
-// mongoose.connect("mongodb://localhost/run_boris_run", {useNewUrlParser:"true"}).catch(function (reason) {
-//   console.log('Unable to connect to the mongodb instance. Error: ', reason);
-// });
+mongoose.connect("mongodb://localhost/run_boris_run", {useNewUrlParser:"true"}).catch(function (reason) {
+  console.log('Unable to connect to the mongodb instance. Error: ', reason);
+});
 
 
 
